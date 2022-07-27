d61f897a402e8e1cc97ac55feace79bf44a06678
Luiz <97242918+nov5lll@users.noreply.github.com>
2022-07-27 22:00:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,41 @@
 from Heun import *
+from RKF45 import *
+from RK4 import *
 import matplotlib.pylab as plt
 # we need to define f, r0, t0, n, h, alpha, k, tol
 
-def f(t,x,y):
+
+# Here we'll define the ODE
+def f(t, r):
+    x, y = r
+
     return np.array([x-x*y, -y+x*y])
 
+
+# Here we need to define the constants, where
+
+# the initial position
 r0 = (10, 10)
 
+# First step of time
 t0 = 0
 
-n = 10000
+# Number of steps
+n = 3000
 
-h = 0.0001
+# lenght of the step
+h = 0.001
 
-t,x,y = odeHeun(f, r0, t0, n, h)
+#t,x,y = odeHeun(f, r0, t0, n, h)
+
+#t,x,y = odeHeun(f2, r0, t0, n, h)
+
+#t,r = odeRK(f, r0, t0, n, h)
+
+t, r, q = odeRKF(f, r0, t0, n, h, 1, 5, 0.001)
+
+x = r[:,0]
+y = r[:,1]
 
 plt.plot(t, x)
 plt.plot(t, y)
