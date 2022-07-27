eb74e4c672db9c7cd3fe0047248d5ad5d6e33866
Luiz <97242918+nov5lll@users.noreply.github.com>
2022-07-27 21:59:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,62 @@
-## Code developed 25 jul 11:09am ##
+from RKF45 import *
+# we need to define f, r0, t0, n, h, alpha, k, tol
 
-# Defining molar mass #
-from cmath import exp
+# Defining the constants:
+A = 3.8*(10**11)
 
+m = 0.25
 
-MW = 1
+n = 1.5
 
-# Defining Cp which's constant #
-Cp = 1
+u = 15.5
 
-# Defining A/F #
-a = 2
+Ru = 8314         #------------- J/kmol.K
 
+Ea = 15098*Ru     #------------- K
 
-####################################
+P = 101325        #------------- Pa
 
-## Equations ##
+deltahc = 440602  #------------- kJ/kg
 
-wf = -A*exp(-Ea/Ru*T)
+Cp = 1685.07      #------------- J/kg.K
 
-
-## Equations ##
-
-####################################
+M = 29            #------------- kg/mol
 
 
-####################################
 
-# Fuel rate #
 
-## needs to be integrate ##
+# Here we'll define the ODE
+def f(t, r):
+    x, y = r
 
-Yi = ((Ru*T)/P)*wi
+    return np.array([x-x*y, -y+x*y])
 
-DT = (-wf*hfF*Ru*T)/(2*Cp*PYf*(1+a))
 
-## needs to be integrate ##
+# Here we need to define the constants, where
 
-####################################
+# the initial position
+r0 = (10, 10)
+
+# First step of time
+t0 = 0
+
+# Number of steps
+n = 3000
+
+# lenght of the step
+h = 0.001
+
+#t,x,y = odeHeun(f, r0, t0, n, h)
+
+#t,x,y = odeHeun(f2, r0, t0, n, h)
+
+#t,r = odeRK(f, r0, t0, n, h)
+
+t, r, q = odeRKF(f, r0, t0, n, h, 1, 5, 0.001)
+
+x = r[:,0]
+y = r[:,1]
+
+plt.plot(t, x)
+plt.plot(t, y)
+plt.show()
